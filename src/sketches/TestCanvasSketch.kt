7a06e4a9565e2c7138d6638b70337ce3373968cd
James Wills <jamesbwills@gmail.com>
2020-12-31 17:51:04 -0800

    update test sketch

--- src/sketches/TestCanvasSketch.kt
+++ src/sketches/TestCanvasSketch.kt
@@ -10,17 +10,26 @@ import util.property2DSlider
 import util.propertySlider
 import util.propertySliderPair
 import util.propertyToggle
+import util.randomColor
 import util.squared
+import util.toRadians
+import kotlin.math.sin
 
 class TestCanvasSketch : CanvasSketch("TestCanvasSketch") {
 
   var boundBoxCenter = Point(0.5, 0.5)
   var boundBoxScale = Point(0.6, 0.8)
   var startAngle = 0.0
-  var angleLength = 100.0
+  var angleLength = 1.0
   var drawBoundRect = true
   var startPoint = Point.Zero
   var endPoint = Point.Zero
+
+  var sinMinAmplitude = 0.0
+  var sinAmplitude = 140.0
+  var sinFreq = 10.0
+
+  var numStars = 3
 
   init {
     markDirty()
@@ -29,6 +38,9 @@ class TestCanvasSketch : CanvasSketch("TestCanvasSketch") {
   override fun getControls(): List<ControlGroup> = listOf(
     *super.getControls().toTypedArray(),
     ControlGroup(
+      propertyToggle(::drawBoundRect)
+    ),
+    ControlGroup(
       *propertySliderPair(::boundBoxCenter)
     ),
     ControlGroup(
@@ -36,9 +48,7 @@ class TestCanvasSketch : CanvasSketch("TestCanvasSketch") {
     ),
     ControlGroup(propertySlider(::startAngle), propertySlider(::angleLength, 0.0..1000.0)),
     ControlGroup(property2DSlider(::startPoint, -1000.0..1000.0, -1000.0..1000.0), property2DSlider(::endPoint, -1000.0..1000.0, -1000.0..1000.0)),
-    ControlGroup(
-      propertyToggle(::drawBoundRect)
-    )
+    ControlGroup(propertySlider(::sinMinAmplitude, 0.0..50.0), propertySlider(::sinAmplitude, 0.0..400.0), propertySlider(::sinFreq, 0.1..100.0), propertySlider(::numStars, 1..10))
   )
 
   override fun drawOnce() {
@@ -47,14 +57,28 @@ class TestCanvasSketch : CanvasSketch("TestCanvasSketch") {
     if (drawBoundRect) rect(boundRect)
 
     val startToEndLine = Segment(center + startPoint, center + endPoint)
-    Spiral(
-      originFunc = { t, percent, deg -> startToEndLine.getPointAtPercent(percent) },
-      lengthFunc = { t, percent, deg ->
-        (t - startAngle).squared()
-      },
-      rotationsRange = startAngle..(angleLength + startAngle))
-      .walk(0.001)
-      .draw(boundRect)
+
+    for (i in 1..numStars) {
+      val starSpacing = 1 / (sinFreq.toInt().toDouble() * numStars)
+
+      val currStartAngle = startAngle + (starSpacing * (i - 1))
+      println(currStartAngle)
+      if (i != 1) {
+        stroke(randomColor())
+      }
+
+      Spiral(
+        originFunc = { t, percent, deg -> startToEndLine.getPointAtPercent(percent) },
+        lengthFunc = { t, percent, deg ->
+          (t).squared()
+          val rad = ((t - currStartAngle) * 360).toRadians()
+          (sin(sinFreq.toInt() * rad) + sinMinAmplitude) * sinAmplitude
+        },
+        rotationsRange = currStartAngle..(angleLength + currStartAngle))
+        .walk(0.0001)
+        .draw(boundRect)
+    }
+
   }
 }
 
