10e12abd5c00e8e2e32b7d586adb9501c2a18122
James Wills <jamesbwills@gmail.com>
2021-03-06 08:35:46 -0800

    Fix svg packages

--- src/util/print/Paper.kt
+++ src/util/print/Paper.kt
@@ -3,15 +3,20 @@ package util.print
 import coordinate.BoundRect
 import coordinate.Point
 import util.lightened
+import util.print.DPI.InkScape
 import util.print.Orientation.Landscape
 import java.awt.Color
+import java.awt.Color.BLACK
+import java.awt.Color.RED
+import java.awt.Color.WHITE
 
 val MAX_VERTICAL_IN = 13.75
 
 // not so sure about max horizontal in
 val MAX_HORIZONTAL_IN = 17.0
 
-enum class DPI(val dpiVal: Int) {
+@Suppress("unused")
+enum class DPI(private val dpiVal: Int) {
   Reg(72),
   InkScape(96),
   High(300);
@@ -20,32 +25,34 @@ enum class DPI(val dpiVal: Int) {
   fun toPixelsFromMm(mm: Number) = toPixels(mm.toDouble() / 25.4)
 }
 
+@Suppress("unused")
 enum class Orientation {
   Portrait,
   Landscape
 }
 
 enum class Paper(
-  val longSideInches: Double,
-  val shortSideInches: Double,
+  private val longSideInches: Double,
+  private val shortSideInches: Double,
   val defaultBackgroundColor: Color,
   val defaultStrokeColor: Color,
 ) {
 
-  LargeWhite(14, 11, Color.WHITE, Color.BLACK),
-  A4White(11.69, 8.27, Color.WHITE, Color.BLACK),
-  A4Black(12, 9, Color.BLACK, Color.WHITE),
-  SquareBlack(7.87, 7.87, Color.BLACK, Color.WHITE),
-  ColoredPaper(12.5, 9.5, Color.RED.lightened(1f), Color.WHITE);
+  LargeWhite(14, 11, WHITE, BLACK),
+  LargeBlack(18, 12, BLACK, WHITE),
+  A4White(11.69, 8.27, WHITE, BLACK),
+  A4Black(12, 9, BLACK, WHITE),
+  SquareBlack(7.87, 7.87, BLACK, WHITE),
+  ColoredPaper(12.5, 9.5, RED.lightened(1f), WHITE);
 
   val defaultStyle: Style = Style(color = defaultStrokeColor)
 
-  private fun sidePx(sideInches: Double, dpi: DPI = DPI.InkScape) = dpi.toPixels(sideInches)
+  private fun sidePx(sideInches: Double, dpi: DPI = InkScape) = dpi.toPixels(sideInches)
 
-  fun horizontalPx(orientation: Orientation, dpi: DPI = DPI.InkScape) =
+  fun horizontalPx(orientation: Orientation, dpi: DPI = InkScape) =
     sidePx(if (orientation == Landscape) longSideInches else shortSideInches, dpi)
 
-  fun verticalPx(orientation: Orientation, dpi: DPI = DPI.InkScape) =
+  fun verticalPx(orientation: Orientation, dpi: DPI = InkScape) =
     sidePx(if (orientation == Landscape) shortSideInches else longSideInches, dpi)
 
   fun toBoundRect(orientation: Orientation) =
@@ -57,9 +64,10 @@ enum class Paper(
     defaultBackgroundColor: Color,
     defaultStrokeColor: Color,
   ) :
-    this(longSideInches.toDouble(),
+    this(
+      longSideInches.toDouble(),
       shortSideInches.toDouble(),
       defaultBackgroundColor,
-      defaultStrokeColor)
-
+      defaultStrokeColor
+    )
 }
